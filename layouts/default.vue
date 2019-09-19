6445c501aed89c3c75875df3d85c8e2fc109de75
cengyongpeng <940391955@qq.com>
2019-09-19 22:41:52 +0800

    init header7.3

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,59 +1,81 @@
 <template>
-  <el-container>
-    <el-header>Header</el-header>
+  <el-container class="layout-default">
+    <el-header>
+      <my-header></my-header>
+    </el-header>
     <el-main>
-      <nuxt />
+      <nuxt></nuxt>
     </el-main>
-    <el-footer>Footer</el-footer>
+    <el-footer>
+      Footer
+    </el-footer>
   </el-container>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<script>
+  import MyHeader from '@/components/public/header/index.vue'
+  export default {
+    name:"default",
+    components:{
+      MyHeader
+    }
+  }
+</script>
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
+<!--<style>-->
+<!--  html {-->
+<!--    font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',-->
+<!--    Roboto, 'Helvetica Neue', Arial, sans-serif;-->
+<!--    font-size: 16px;-->
+<!--    word-spacing: 1px;-->
+<!--    -ms-text-size-adjust: 100%;-->
+<!--    -webkit-text-size-adjust: 100%;-->
+<!--    -moz-osx-font-smoothing: grayscale;-->
+<!--    -webkit-font-smoothing: antialiased;-->
+<!--    box-sizing: border-box;-->
+<!--  }-->
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
+<!--  *,-->
+<!--  *:before,-->
+<!--  *:after {-->
+<!--    box-sizing: border-box;-->
+<!--    margin: 0;-->
+<!--  }-->
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
+<!--  .button&#45;&#45;green {-->
+<!--    display: inline-block;-->
+<!--    border-radius: 4px;-->
+<!--    border: 1px solid #3b8070;-->
+<!--    color: #3b8070;-->
+<!--    text-decoration: none;-->
+<!--    padding: 10px 30px;-->
+<!--  }-->
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
+<!--  .button&#45;&#45;green:hover {-->
+<!--    color: #fff;-->
+<!--    background-color: #3b8070;-->
+<!--  }-->
 
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+<!--  .button&#45;&#45;grey {-->
+<!--    display: inline-block;-->
+<!--    border-radius: 4px;-->
+<!--    border: 1px solid #35495e;-->
+<!--    color: #35495e;-->
+<!--    text-decoration: none;-->
+<!--    padding: 10px 30px;-->
+<!--    margin-left: 15px;-->
+<!--  }-->
+
+<!--  .button&#45;&#45;grey:hover {-->
+<!--    color: #fff;-->
+<!--    background-color: #35495e;-->
+<!--  }-->
+<!--</style>-->
+
+
+
+
+
+
+
+
