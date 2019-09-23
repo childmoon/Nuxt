799c1376ff4fc367a28c9cf70b8b275bf8e74e9a
cengyongpeng <940391955@qq.com>
2019-09-24 00:31:57 +0800

    only 9.3 import local database model2

--- server/interface/geo.js
+++ server/interface/geo.js
@@ -2,6 +2,7 @@ import Router from 'koa-router';
 import axios from './utils/axios'
 import Province from '../dbs/models/province'
 import City from '../dbs/models/city'
+import Menu from '../dbs/models/menu'
 
 let router = new Router({prefix: '/geo'})
 
@@ -144,22 +145,22 @@ router.get('/hotCity', async (ctx) => {
 })
 
 router.get('/menu', async (ctx) => {
-  // const result = await Menu.findOne()
-  // ctx.body = {
-  //   menu: result.menu
-  // }
-  let {status, data: {
-      menu
-    }} = await axios.get(`http://cp-tools.cn/geo/menu?sign=${sign}`);
-  if (status === 200) {
-    ctx.body = {
-      menu
-    }
-  } else {
-    ctx.body = {
-      menu: []
-    }
+  const result = await Menu.findOne()
+  ctx.body = {
+    menu: result.menu
   }
+  // let {status, data: {
+  //     menu
+  //   }} = await axios.get(`http://cp-tools.cn/geo/menu?sign=${sign}`);
+  // if (status === 200) {
+  //   ctx.body = {
+  //     menu
+  //   }
+  // } else {
+  //   ctx.body = {
+  //     menu: []
+  //   }
+  // }
 })
 
 export default router;
